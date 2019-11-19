476fab03b11eb9db2368ce691502154223c6b86d
Alan Nguyen <nguyenalan@berkeley.edu>
2019-11-19 13:31:58 -0800

    serve static files, create /images endpoint

--- server/app.js
+++ server/app.js
@@ -1,18 +1,25 @@
 var express = require("express");
-var terms = require("./assets/json/diseases.json");
+var terms = require("./assets/diseases.json");
+var studies = require("./assets/studies.json");
 var app = express();
 
 app.listen(3000, () => {
     console.log("Server running on port 3000.");
 });
 
-app.use('/images', express.static(path.join(__dirname, 'assets/images')))
+app.use('/images', express.static('images'));
+
+app.get("/images", (req, res, next) => {
+    res.json(studies);
+});
 
 app.get("/diseases", (req, res, next) => {
     let d = {
         diseases: terms
     };
     res.json(d);
-})
+});
+
+// do post request after building front-end
 
 
