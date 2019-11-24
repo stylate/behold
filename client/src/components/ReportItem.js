f58c8aeba8c4a48e4eb576d631e12ea4877a4c4d
Alan Nguyen <nguyenalan@berkeley.edu>
2019-11-24 13:46:58 -0800

    fixed toggle bug in reportitem

--- client/src/components/ReportItem.js
+++ client/src/components/ReportItem.js
@@ -6,14 +6,14 @@ const Text = styled.p``;
 
 const ReportItem = (props) => {
     const { item, toggle } = props;
-    console.log("item UPDATE: ", item);
     const toggleItem = () => {
         toggle(item.id)
     };
+    console.log("item UPDATE: ", item);
     return (
         <Grid>
             <Grid.Column key={1} width={9}><Text>{item.id}</Text></Grid.Column>
-            <Grid.Column key={2}><Checkbox toggle defaultChecked={item.exists} onChange={toggleItem}/></Grid.Column>
+            <Grid.Column key={2}><Checkbox toggle checked={item.exists} onClick={toggleItem}/></Grid.Column>
         </Grid>
     );
 }
